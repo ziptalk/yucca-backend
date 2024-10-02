fcf1fc52d664955ae5050bf673a2aeda5f4cebde
김시영 <siyeong0926@naver.com>
2024-10-02 11:24:07 +0900

    대시보드 수정 Ver12

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -10,4 +10,4 @@ const UserSchema = new Schema<iUser>({
     stakeAmount: Number,
 },{collection : 'user'});
 
-export const User = mongoose.model<iUser>('user', UserSchema);
+export const User = mongoose.model<iUser>('User', UserSchema);
