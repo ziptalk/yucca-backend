7cf0a64af3f5564f38220467f5c6d93263a0ccd6
leeeunda <daun0219@gmail.com>
2024-10-10 17:40:58 +0900

    fix: remove HTTPS process

--- src/server.ts
+++ src/server.ts
@@ -13,19 +13,3 @@ app.listen(HTTP_PORT, () => {
         await saveBotBalance();
     });
 });
-
-const domainName = process.env.DOMAIN_NAME;
-try {
-    const keyFile = fs.readFileSync(`/etc/letsencrypt/live/${domainName}/privkey.pem`);
-    const certFile = fs.readFileSync(`/etc/letsencrypt/live/${domainName}/fullchain.pem`);
-    const options = {
-        key: keyFile,
-        cert: certFile,
-    };
-
-    https.createServer(options, app).listen(HTTPS_PORT, () => {
-        console.log(`Server running at https://localhost:${HTTPS_PORT}`);
-    });
-} catch (error) {
-    console.error(`There was a problem while running the server on HTTPS - message: ${error}`);
-}
