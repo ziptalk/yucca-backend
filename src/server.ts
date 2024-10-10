afe250e11f80c6c603a5ff3a0bf4feb307e46155
leeeunda <daun0219@gmail.com>
2024-10-10 14:40:31 +0900

    feat: connect domain

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import app from './app';
+import fs from "fs";
+import https from "https";
 import {scheduleJob} from 'node-schedule';
 import {saveBotBalance} from './services/balanceService';
 
@@ -11,19 +13,19 @@ app.listen(HTTP_PORT, () => {
         await saveBotBalance();
     });
 });
-//
-// const domainName = process.env.DOMAIN_NAME;
-// try {
-//     const keyFile = fs.readFileSync(`/etc/letsencrypt/live/${domainName}/privkey.pem`);
-//     const certFile = fs.readFileSync(`/etc/letsencrypt/live/${domainName}/fullchain.pem`);
-//     const options = {
-//         key: keyFile,
-//         cert: certFile,
-//     };
-//
-//     https.createServer(options, app).listen(HTTPS_PORT, () => {
-//         console.log(`Server running at https://localhost:${HTTPS_PORT}`);
-//     });
-// } catch (error) {
-//     console.error(`There was a problem while running the server on HTTPS - message: ${error}`);
-// }
+
+const domainName = process.env.DOMAIN_NAME;
+try {
+    const keyFile = fs.readFileSync(`/etc/letsencrypt/live/${domainName}/privkey.pem`);
+    const certFile = fs.readFileSync(`/etc/letsencrypt/live/${domainName}/fullchain.pem`);
+    const options = {
+        key: keyFile,
+        cert: certFile,
+    };
+
+    https.createServer(options, app).listen(HTTPS_PORT, () => {
+        console.log(`Server running at https://localhost:${HTTPS_PORT}`);
+    });
+} catch (error) {
+    console.error(`There was a problem while running the server on HTTPS - message: ${error}`);
+}
